14c9e292c1532b4be58c9b261c42b2541f703885
Miguel Pedraza <mikeped469@gmail.com>
2023-09-07 14:59:23 -0600

    Added basic general styles for index css and basic header and todo list component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+// Styles
+import "./Header.css";
+
+const Header = () => {
+	return (
+		<header>
+			<h1>To-Do App</h1>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,12 @@
+// Styles
+import "./TodoList.css";
+
+const TodoList = () => {
+	return (
+		<div>
+			<h1>Todolist</h1>
+		</div>
+	);
+};
+
+export default TodoList;
